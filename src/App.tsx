9bd0c7940a4876f0554acb8b19510ab5aabfba3b
Szymon Niewiadomski <87074246+0SAS0@users.noreply.github.com>
2025-05-26 18:50:13 +0200

    final

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { SyntheticEvent, useRef, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import { List } from './list.type.ts';
 import { toast, Toaster } from 'sonner';
+import { twMerge } from 'tailwind-merge';
 
 const App = () => {
   const [value, setValue] = useState<string>('');
@@ -10,7 +11,6 @@ const App = () => {
   const handleInput = (value: string) => {
     setValue(value);
   };
-
   const handleSubmit = (event: SyntheticEvent<HTMLFormElement>) => {
     event.preventDefault();
     if (value.trim() === '') {
@@ -43,40 +43,45 @@ const App = () => {
   };
   return (
     <div className={'bg-zinc-100 min-h-screen flex items-start justify-center py-8'}>
-      <div
-        className={
-          'w-5xl bg-zinc-200 rounded-lg p-4 flex items-center justify-center flex-wrap gap-2 flex-col'
-        }
-      >
+      <div className={'w-5xl bg-zinc-200 rounded-lg p-4 flex  flex-wrap gap-2 flex-col'}>
         <h1 className={'text-center text-5xl font-bold text-zinc-700'}>TODO LIST</h1>
+        <p>Szymon Niewiadomski 122861</p>
         <form className={'flex flex-wrap gap-2 w-full'} onSubmit={handleSubmit}>
           <input
             placeholder={'Task...'}
             className={
-              'bg-zinc-100 rounded outline-none focus:ring-2 focus:ring-zinc-700 py-1.5 px-2.5 flex-1'
+              'bg-zinc-100 rounded-md outline-none focus:ring-2 focus:ring-zinc-700 py-1.5 px-2.5 flex-1'
             }
             value={value}
             ref={inputRef}
             onChange={(e) => handleInput(e.target.value)}
           />
           <button
-            className={'rounded bg-blue-500 py-1.5 px-6 text-white font-bold hover:bg-blue-600'}
+            className={'rounded-md bg-blue-500 py-1.5 px-6 text-white font-bold hover:bg-blue-600'}
           >
             Add
           </button>
         </form>
-        <ul>
+        <ul className={'w-full gap-2 grid'}>
           {list.map((item) => (
-            <li key={item.uuid}>
-              {item.value}
+            <li
+              className={
+                'gap-1.5 grid grid-cols-[auto_1fr_auto] items-center bg-zinc-100 px-2 py-3 rounded-md'
+              }
+              key={item.uuid}
+            >
               <input
-                className={'size-6 '}
+                className={twMerge(
+                  'size-9 bg-zinc-200',
+                  item.isDone ? 'appearance-auto' : 'appearance-none',
+                )}
                 checked={item.isDone}
                 type={'checkbox'}
                 onChange={() => handleCheck(item.uuid)}
               />
+              <span className={'break-all'}>{item.value}</span>
               <button
-                className={'rounded bg-red-500 py-1.5 px-6 text-white font-bold hover:bg-red-700'}
+                className={'rounded bg-red-500 h-9 px-6 text-white font-bold hover:bg-red-700'}
                 onClick={() => handleDelete(item.uuid)}
               >
                 Delete
@@ -85,7 +90,6 @@ const App = () => {
           ))}
         </ul>
       </div>
-
       <Toaster richColors position="top-center" />
     </div>
   );
